195af96349b86f4c9a737160619154a6cdd215d0
Dylan Toyne <dylantoyne@gmail.com>
2017-01-16 22:21:53 +0000

    URL is working and pointing to github repo.

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,18 @@
 var request = require('request');
+var GITHUB_USER = "Dtoyne";
+var GITHUB_TOKEN = "4e8b9e4d4dc8bb62ddce6b9d27e6c10d2c9c12fa";
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + GITHUB_USER + '/' + "github-avatar-downloader" + '/contributors';
+  console.log(requestURL);
 }
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
-});
+getRepoContributors();
+
+
+// getRepoContributors("jquery", "jquery", function(err, result) {
+//   console.log("Errors:", err);
+//   console.log("Result:", result);
+// });
