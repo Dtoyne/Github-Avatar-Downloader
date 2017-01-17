eecb694fbf9647dc1484c1da1ef0a8f900bffe83
Dylan Toyne <dylantoyne@gmail.com>
2017-01-17 02:04:01 +0000

    Cleaned up code, added comments for users, and ultimately revised for submission. Incorporation of stretch goals will be included in next commit.

--- download_avatars.js
+++ download_avatars.js
@@ -1,47 +1,85 @@
-// console.log('Welcome to the GitHub Avatar Downloader!');
 
-var request = require('request');
-var fs = require('fs');
-var https = require('https');
-var GITHUB_USER = "Dtoyne";
-var GITHUB_TOKEN = "4e8b9e4d4dc8bb62ddce6b9d27e6c10d2c9c12fa";
-var owner = process.argv[2];
-var repo = process.argv[3];
+// Log greeting message:
 
-function getRepoContributors(repoOwner, repoName, cb) {
-  var requestURL = ('https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors')
-  var https = require("https");
-  var fs = require("fs");
-  var options = {
-    url: requestURL,
-    headers: {
-    'User-Agent': 'request'
-  }
-};
+console.log('Welcome to the GitHub Avatar Downloader!');
 
-  request.get(options, function(error, response, body) {
-    if (error) {
-      return error;
-    }
-    if (response.statusCode === 200) {
-      var data = JSON.parse(body);
-      cb(null, data);
-    }
-  });
-};
+let request = require("request");
+let fs = require("fs");
+let https = require("https");
+let GITHUB_USER = "Dtoyne";
+let GITHUB_TOKEN = "4e8b9e4d4dc8bb62ddce6b9d27e6c10d2c9c12fa";
+let owner = process.argv[2];
+let repo = process.argv[3];
+let args = process.argv;
 
-getRepoContributors(owner, repo, function(err, result) {
-  if (err) {
-    console.log("Errors:", err);
-    return err;
-  }
-  for (var i = 0; i < result.length; i++) {
-    downloadImageByURL(result[i].avatar_url, "./avatars/" + result[i].login + ".jpg")
-  }
+// Based on the command-line input, get the list of contributors from the GitHub API:
+
+function getRepoContributors (repoOwner, repoName, cb) {
+
+  let requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  let https = require("https");
+  let fs = require("fs");
+  let options = {
+    "url": requestURL,
+    "headers": {
+      "User-Agent": 'request'}
+  };
+
+    if (repo === undefined) {
+
+      throw "You need to input some arguments";
+
+} else if (process.argv.length > 4) {
+
+      throw "You put in too many arguments";
+
+}
+
+    request.get(options, (error, response, body) => {
+
+      if (error) {
+
+        return error;
+
+}
+      if (response.statusCode === 200) {
+
+      let data = JSON.parse(body);
+        cb(null, data);
+
+}
+
 });
 
-function downloadImageByURL(url, filepath) {
-  request.get(url, function(response) {
-  })
-  .pipe(fs.createWriteStream(filepath));
+}
+
+// Loop through the contributors and download their respective avatars:
+
+getRepoContributors(owner, repo, (err, result) => {
+
+    if (err) {
+
+      console.log("Errors:", err);
+
+return err;
+
+}
+    for (let i = 0; i < result.length; i++) {
+
+      downloadImageByURL(result[i].avatar_url, `./avatars/${  result[i].login  }.jpg`);
+
+}
+
+});
+
+// Pipe the .jpg's into the avatars folder in the directory:
+
+function downloadImageByURL (url, filepath) {
+
+    request.get(url, (response) => {
+
+  }).
+
+  pipe(fs.createWriteStream(filepath));
+
 }
