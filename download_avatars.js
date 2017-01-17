871e6e8b58f06c20b1a87a80405334226078e60c
Dylan Toyne <dylantoyne@gmail.com>
2017-01-17 00:33:14 +0000

    initiated new function that allowed us to download / pipe an image to the avatars folder

--- download_avatars.js
+++ download_avatars.js
@@ -2,12 +2,15 @@
 
 var request = require('request');
 var fs = require('fs');
+var https = require('https');
 var GITHUB_USER = "Dtoyne";
 var GITHUB_TOKEN = "4e8b9e4d4dc8bb62ddce6b9d27e6c10d2c9c12fa";
 
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var requestURL = ('https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors')
+  var https = require("https");
+  var fs = require("fs");
   var options = {
     url: requestURL,
     headers: {
@@ -36,3 +39,11 @@ getRepoContributors("jquery", "jquery", function(err, result) {
     console.log(result[i].avatar_url);
   }
 });
+
+function downloadImageByURL(url, filepath) {
+  request.get(url, function(response) {
+  })
+  .pipe(fs.createWriteStream(filepath));
+}
+
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
